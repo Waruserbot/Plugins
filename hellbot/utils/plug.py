2bb9da7f96675259eabc1d79d5c0e1dda476e625
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-08 17:41:40 +0530

    Update plug.py

--- hellbot/utils/plug.py
+++ hellbot/utils/plug.py
@@ -87,7 +87,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["hellbot.plugins." + shortname] = mod
-        LOGS.info("⚡ Hêllẞø† ⚡ - Successfully Imported " + shortname)
+        LOGS.info("⚡ WarUserBot ⚡ - Successfully Imported " + shortname)
 
 
 # remove plugins
@@ -111,8 +111,8 @@ def remove_plugin(shortname):
 
 async def plug_channel(client, channel):
     if channel:
-        LOGS.info("⚡ Hêllẞø† ⚡ - PLUGIN CHANNEL DETECTED.")
-        LOGS.info("⚡ Hêllẞø† ⚡ - Starting to load extra plugins.")
+        LOGS.info("⚡ WarUserBot ⚡ - PLUGIN CHANNEL DETECTED.")
+        LOGS.info("⚡ WarUserBot ⚡ - Starting to load extra plugins.")
         plugs = await client.get_messages(channel, None, filter=InputMessagesFilterDocument)
         total = int(plugs.total)
         for plugins in range(total):
