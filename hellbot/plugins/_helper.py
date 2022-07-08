2432c68b03e87f6b1c9aa2bce71c7b02369365b7
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-08 16:50:09 +0530

    Update _helper.py

--- hellbot/plugins/_helper.py
+++ hellbot/plugins/_helper.py
@@ -8,9 +8,9 @@ from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot, BotInl
 from . import *
 
 msg = f"""
-**⚡ ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt ⚡**
-  •        [📑 Repo 📑](https://github.com/The-HellBot/HellBot)
-  •        [HellBot Network](https://t.me/hellbot_network)
+**⚡ Legendary Af waruserbot ⚡**
+  •        [📑 Repo 📑](https://github.com/MrAbhish3k/waruserbot)
+  •        [Waruserbot Support](https://t.me/waruserbotsupport)
   •  ©️ {hell_channel} ™
 """
 botname = Config.BOT_USERNAME
