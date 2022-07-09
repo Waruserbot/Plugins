49cd4f3d6dc83d0473b26b4b7545a19763632238
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-09 10:51:41 +0530

    Update alive.py

--- hellbot/plugins/alive.py
+++ hellbot/plugins/alive.py
@@ -76,7 +76,7 @@ async def hell_a(event):
 CmdHelp("alive").add_command(
   "alive", None, "Shows the Default Alive Message"
 ).add_command(
-  "hell", None, "Shows Inline Alive Menu with more details."
+  "war", None, "Shows Inline Alive Menu with more details."
 ).add_warning(
   "✅ Harmless Module"
 ).add()
