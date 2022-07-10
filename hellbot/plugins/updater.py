f8a81438b5db792cd274b36d999da67404a0a7a4
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-10 22:27:56 +0530

    Update updater.py

--- hellbot/plugins/updater.py
+++ hellbot/plugins/updater.py
@@ -23,18 +23,18 @@ NO_HEROKU_APP_CFGD = "No Heroku App Found!"
 HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
 RESTARTING_APP = "Restarting Heroku App..."
 IS_SELECTED_DIFFERENT_BRANCH = "Looks like a custom branch {branch_name} is being used!\nIn this case, updater is unable to identify the branch to be updated. Please check out to an official branch, and re-start the updater."
-hellbot_info = "https://raw.githubusercontent.com/waruserbot/Plugins/master/hellbot-info.json"
+hellbot_info = "https://raw.githubusercontent.com/waruserbot/Plugins/master/warbot-info.json"
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 requirements_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "requirements.txt")
 
 
-async def hell_info(hellbot_info):
+async def war_info(warbot_info):
     infos = requests.get(hellbot_info).json()
-    _version = infos['HELLBOT-INFO']['version']
-    _release = infos['HELLBOT-INFO']['release-date']
-    _branch = infos['HELLBOT-INFO']['branch']
-    _author = infos['HELLBOT-INFO']['author']
-    _auturl = infos['HELLBOT-INFO']['author-url']
+    _version = infos['WARBOT-INFO']['version']
+    _release = infos['WARBOT-INFO']['release-date']
+    _branch = infos['WARBOT-INFO']['branch']
+    _author = infos['WARBOT-INFO']['author']
+    _auturl = infos['WARBOT-INFO']['author-url']
     return _version, _release, _branch, _author, _auturl
 
 
