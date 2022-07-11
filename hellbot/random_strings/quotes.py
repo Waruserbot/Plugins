3810414cc3b1a05a89abf419c5034d1331a8ffdb
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-11 09:59:29 +0530

    Update quotes.py

--- hellbot/random_strings/quotes.py
+++ hellbot/random_strings/quotes.py
@@ -45,7 +45,7 @@ METOOSTR = [
     "Me rn",
 ]
 
-perf = "[ †hê Hêllẞø† ]"
+perf = "[ᴡᴀʀᴜsᴇʀʙᴏᴛ]"
 
 GDNOON = [
     "`My wishes will always be with you, Morning wish to make you feel fresh, Afternoon wish to accompany you, Evening wish to refresh you, Night wish to comfort you with sleep, Good Afternoon Dear!`",
