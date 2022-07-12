137075bb50e7d6641549599ad28f26b75031099c
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-12 06:41:50 +0530

    Update archiver.py

--- hellbot/plugins/archiver.py
+++ hellbot/plugins/archiver.py
@@ -90,7 +90,7 @@ async def _(event):
     await event.client.send_file(
         event.chat_id,
         directory_name + ".zip",
-        caption="Zipped By HellBot",
+        caption="Zipped By WarBot",
         force_document=True,
         allow_cache=False,
         reply_to=event.message.id,
@@ -132,7 +132,7 @@ async def _(event):
             await event.client.send_file(
                 event.chat_id,
                 directory_name + ".rar",
-                caption="rarred By HellBot",
+                caption="rarred By WarBot",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
@@ -179,7 +179,7 @@ async def _(event):
             await event.client.send_file(
                 event.chat_id,
                 directory_name + ".7z",
-                caption="7z archived By HellBot",
+                caption="7z archived By WarBot",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
@@ -228,7 +228,7 @@ async def _(event):
             await event.client.send_file(
                 event.chat_id,
                 output,
-                caption="TAR By HellBot",
+                caption="TAR By WarBot",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
