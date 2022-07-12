e44d97cd15bba688c18d25d1fd4cb75cfed35ea5
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-12 10:07:31 +0530

    Add files via upload

---
+++ warbot/random_strings/devs.py
@@ -0,0 +1,17 @@
+DEVLIST = ["1891078417,2097320259,1329105215"]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "STRING_SESSION",
+    "session.save()",
+]
