9947fb340e25adb29b9da3283ab5f0e02f506c83
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-12 12:27:07 +0530

    Update mmf.py

--- warbot/helpers/mmf.py
+++ warbot/helpers/mmf.py
@@ -14,7 +14,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "warbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
@@ -121,7 +121,7 @@ async def draw_meme(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "warbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
