8c34126d905ac6448f03bb36dbdd1ca537d9d1ba
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-12 14:15:32 +0530

    Update pmpermit.py

--- warbot/plugins/pmpermit.py
+++ warbot/plugins/pmpermit.py
@@ -5,7 +5,7 @@ import os
 from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
-from hellbot.sql import pmpermit_sql as pm_sql
+from warbot.sql import pmpermit_sql as pm_sql
 from . import *
 
 PM_WARNS = {}
